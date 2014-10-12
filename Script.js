b3e38b4a47f0ab9a60027c0129c513212eaec408
canuk790 <jer.p.scott@gmail.com>
2014-10-12 16:21:31 -0700

    First commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Scott Industries Maximal. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='Index.html'>Home</a></li>" +
+"<li><a href='About.html'>About</a></li>" +
+"<li><a href='Employees.html'>Employees</a></li>" +
+"</ul>";
