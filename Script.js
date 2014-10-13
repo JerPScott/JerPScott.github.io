789c55f03aab52ed38a0436e6a14946e156e9329
canuk790 <jer.p.scott@gmail.com>
2014-10-13 11:22:26 -0700

    Site redesign. Now an About Me page.

--- Script.js
+++ Script.js
@@ -1,8 +1,10 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Scott Industries Maximal. All rights reserved.</p>";
+"<p>  " + new Date().getFullYear() + " Made by Jeremy Scott</p>";
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='Index.html'>Home</a></li>" +
-"<li><a href='About.html'>About</a></li>" +
-"<li><a href='Employees.html'>Employees</a></li>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='travel.html'>Travel</a></li>"+
+"<li><a href='programming.html'>Programming</a></li>" +
+"<li><a href='snowboarding.html'>Snowboarding</a></li>" +
+"<li><a href='hiking.html'>Hiking/Camping</a></li>" +
 "</ul>";
